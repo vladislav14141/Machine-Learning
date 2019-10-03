801b057753b6940f6908bcf994975d3e1c93b6fa
Vladislav <vladislav14141@yandex.ru>
2019-10-03 15:36:51 +1000

    Initial Commit

---
+++ Machine Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine Learning
+//
+//  Created by Миронов Влад on 03.10.2019.
+//  Copyright © 2019 Миронов Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
